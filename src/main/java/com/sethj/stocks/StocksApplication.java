d8332d052dd0fb6c49660428d953818d7dd2c9fa
Seth Jones <sethjones348@gmail.com>
2022-04-18 21:26:39 -0400

    Working using the inferior API

--- src/main/java/com/sethj/stocks/StocksApplication.java
+++ src/main/java/com/sethj/stocks/StocksApplication.java
@@ -2,12 +2,8 @@ package com.sethj.stocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import controller.StockController;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses={StockController.class})
 public class StocksApplication {	
 	public static void main(String[] args) {
 		SpringApplication.run(StocksApplication.class, args);
